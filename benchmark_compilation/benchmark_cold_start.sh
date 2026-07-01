c60f9a3197ddc511b471bf10d878ba731179ecfd
gabrieldemarmiesse <gabriel@kyutai.org>
2026-07-01 17:13:25 +0200

    trim: remove docs/scripts/CI/compat (bug still reproduces)

--- benchmark_compilation/benchmark_cold_start.sh
+++
@@ -1,9 +0,0 @@
-set -ex
-
-docker build -t benchmark-cold-start -f benchmark_compilation/Dockerfile .
-
-docker run --rm \
-    --device=/dev/kfd --device=/dev/dri \
-    --group-add video --group-add render \
-    --security-opt seccomp=unconfined \
-    benchmark-cold-start
